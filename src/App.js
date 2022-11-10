e9497803e8f2626a92c7ecbfb220fd94849418ca
Rdhwnzaki <zaki85.rmz@gmail.com>
2022-11-10 20:14:04 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import shopLogo from "./image/logo.png";
+import vector from "./image/Vector.png";
+import cart from "./image/shopping-cart.png";
+// import Login from "./pages/Login";
+import { BrowserRouter, Route, Link, Routes, Navigate } from "react-router-dom";
 
 function App() {
+  const [title, setTitle] = useState("Belajar Frontend");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">{title}</header>
+      <BrowserRouter>
+        <nav className="navbar navbar-expand-lg navbar-white bg-white">
+          <div className="container text-center">
+            <div className="row align-items-center">
+              <img src={shopLogo} alt="" id="logo--nav" />
+              <h3
+                className="text-danger myfont"
+                style={{ margintop: "20px", marginleft: "10px" }}
+              >
+                Shop.id
+              </h3>
+              <form className="form-inline my-2 my-lg-0">
+                <input
+                  className="form-control mr-sm-2 myfont2"
+                  type="search"
+                  style={{
+                    margintop: "23px",
+                    marginbottom: "15px",
+                    marginleft: "30px",
+                    width: "400px",
+                    borderradius: "30px",
+                    height: "45px",
+                  }}
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+              </form>
+              <button
+                className="btn btn-outline-dark btn-block btn-md"
+                id=""
+                style={{
+                  margintop: "23px",
+                  marginbottom: "15px",
+                  height: "40px",
+                  borderradius: "10px",
+                  marginleft: "3px",
+                  width: "50px",
+                }}
+              >
+                <img src={vector} alt="" />
+              </button>
+              <img
+                src={cart}
+                style={{
+                  margintop: "20px",
+                  marginleft: "135px",
+                  marginbottom: "15px",
+                }}
+                alt=""
+              />
+              <button
+                className="btn btn-danger btn-block btn-md myfont2"
+                id="button-cust-2"
+                style={{
+                  margintop: "23px",
+                  marginbottom: "15px",
+                  height: "45px",
+                  borderradius: "40px",
+                  marginleft: "20px",
+                }}
+              >
+                {/* <Link to="/login">Login</Link> */}
+              </button>
+
+              <a href="regisCustommer.html" id="link">
+                <button
+                  className="btn btn-gray btn-outline-danger btn-block btn-md myfont2"
+                  id="button-cust-2"
+                  style={{
+                    margintop: "23px",
+                    marginbottom: "15px",
+                    height: "45px",
+                    borderradius: "40px",
+                    marginleft: "20px",
+                  }}
+                >
+                  Signup
+                </button>
+              </a>
+            </div>
+          </div>
+        </nav>
+        <Routes>{/* <Route path="/login" element={<Login />} /> */}</Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Product/index.js
@@ -0,0 +1,130 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styles from "./Product.module.css";
+
+export default function Product() {
+  const [data, setData] = useState([]);
+  const [photo, setPhoto] = useState(null);
+  const [inputData, setInputData] = useState({
+    name_product: "",
+    stock_product: "",
+    price_product: "",
+    category_id: "1",
+  });
+
+  let product = "http://localhost:3000/products/";
+  useEffect(() => {
+    axios
+      .get(product)
+      .then((res) => {
+        console.log("get data success");
+        console.log(res.data.product);
+        res.data && setData(res.data.product);
+      })
+      .catch((err) => {
+        console.log("get data fail");
+        console.log(err);
+      });
+  }, []);
+  const postForm = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", inputData.name);
+    formData.append("stock", inputData.stock);
+    formData.append("price", inputData.price);
+    formData.append("category_id", inputData.category_id);
+    formData.append("photo", photo);
+    console.log(formData);
+    axios
+      .post("http://localhost:3000/products", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        console.log("input data success");
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log("input data fail");
+        console.log(err);
+      });
+  };
+  const handlePhoto = (e) => {
+    setPhoto(e.target.files[0]);
+    console.log(e.target.files[0]);
+  };
+
+  const handleChange = (e) => {
+    setInputData({
+      ...inputData,
+      [e.target.name]: e.target.value,
+    });
+    console.log(data);
+  };
+
+  return (
+    <div>
+      {/* post data */}
+      <form onSubmit={postForm} className="container mt-3 p-2 border">
+        <input
+          type="text"
+          value={inputData.name}
+          name="name"
+          onChange={handleChange}
+          placeholder="nama"
+        />
+        <input
+          type="number"
+          value={inputData.stock}
+          name="stock"
+          onChange={handleChange}
+          placeholder="stock"
+        />
+        <input
+          type="number"
+          value={inputData.price}
+          name="price"
+          onChange={handleChange}
+          placeholder="price"
+        />
+        <input
+          type="file"
+          name="photo"
+          onChange={handlePhoto}
+          placeholder="photo"
+        />
+
+        <button className="btn btn-primary" type="submit">
+          input
+        </button>
+      </form>
+
+      {/* get data */}
+      <table className="table">
+        <thead>
+          <tr>
+            <th>number</th>
+            <th>nama</th>
+            <th>stock</th>
+            <th>harga</th>
+            <th>photo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index + 1}>
+              <td>{index + 1}</td>
+              <td>{item.name_product}</td>
+              <td>{item.stock_product}</td>
+              <td>{item.price_product}</td>
+              <td>
+                <img src={item.photo} className={styles.photo} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
